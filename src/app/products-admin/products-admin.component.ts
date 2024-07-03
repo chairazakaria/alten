aa80f337d76b680cee83020d98926fc3b0c32007
zchaira <zakaria.chaira.external@spherea.com>
2024-07-03 11:57:00 +0200

    add dialog confirm

--- src/app/products-admin/products-admin.component.ts
+++ src/app/products-admin/products-admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'app/model/product';
 import { ProductService } from 'app/services/productservice';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-products-admin',
@@ -16,11 +16,16 @@ export class ProductsAdminComponent implements OnInit {
   submitted: boolean; 
   productDialog: boolean;
 
-  constructor(private productService: ProductService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
+  constructor(private productService: ProductService, private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
       this.productService.getProducts().then(data => this.products = data); 
   }
+
+  hideDialog() {
+    this.productDialog = false;
+    this.submitted = false;
+}
 
   openNew() {
     this.product = {};
@@ -28,17 +33,43 @@ export class ProductsAdminComponent implements OnInit {
     this.productDialog = true;
 }
 
-deleteSelectedProducts() { 
-  this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-  this.selectedProducts = null; 
+deleteSelectedProducts() {  
+  this.confirmationService.confirm({
+    message: 'Are you sure you want to delete the selected products?',
+    header: 'Confirm',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+      this.products = this.products.filter(val => !this.selectedProducts.includes(val));
+      this.selectedProducts = null; 
+    }
+});
 }
 
 editProduct(product: Product) {
-  this.product = {...product};
-  this.productDialog = true;
+  /* this.product = {...product};
+  this.productDialog = true; */
+  this.confirmationService.confirm({
+    message: 'Are you sure you want to delete ' + product.name + '?',
+    header: 'Confirm',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => { 
+    }
+});
 }
-deleteProduct(product: Product) { 
-  this.products = this.products.filter(val => val.id !== product.id);
+deleteProduct(product: Product) {   
+  this.confirmationService.confirm({
+    message: 'Are you sure you want to delete ' + product.name + '?',
+    header: 'Confirm',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+        this.products = this.products.filter(val => val.id !== product.id);
+        this.product = {}; 
+    }
+});
+}
+
+saveProduct(){
+  
 }
 
 }
